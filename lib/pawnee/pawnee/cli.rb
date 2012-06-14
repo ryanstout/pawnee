bed8cdb394f707f3bbf166ce654e49c9e54f7c52
Ryan Stout <ryanstout@gmail.com>
2012-06-14 17:30:04 -0600

    Add spec dir for generated gems

--- lib/pawnee/pawnee/cli.rb
+++ lib/pawnee/pawnee/cli.rb
@@ -55,9 +55,12 @@ module Pawnee
       template(File.join("newgem/README.md.tt"),             File.join(target, "README.md"),              opts)
       template(File.join("newgem/gitignore.tt"),             File.join(target, ".gitignore"),             opts)
       template(File.join("newgem/newgem.gemspec.tt"),        File.join(target, "#{name}.gemspec"),        opts)
-      template(File.join("newgem/lib/pawnee/newgem.rb.tt"), File.join(target, "lib/pawnee/#{name}.rb"),         opts)
+      template(File.join("newgem/lib/pawnee/newgem.rb.tt"),  File.join(target, "lib/pawnee/#{name}.rb"),         opts)
       template(File.join("newgem/lib/pawnee/newgem/version.rb.tt"), File.join(target, "lib/#{folder_name}/version.rb"), opts)
       template(File.join("newgem/lib/pawnee/newgem/base.rb.tt"), File.join(target, "lib/#{folder_name}/base.rb"), opts)
+      template(File.join("newgem/spec/spec_helper.rb.tt"),   File.join(target, "spec/spec_helper.rb"), opts)
+      template(File.join("newgem/spec/vagrant/Vagrantfile.tt"),     File.join(target, "spec/vagrant/Vagrantfile"), opts)
+      template(File.join("newgem/spec/vagrant/vagrant.rb.tt"),      File.join(target, "spec/vagrant/vagrant.rb"), opts)
       if options[:bin]
         template(File.join("newgem/bin/newgem.tt"),          File.join(target, 'bin', name),              opts)
       end
